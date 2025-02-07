0b0f73531561739de596e24c27d4b9a1bacf6268
Muhammad Umair <93200938+umair763@users.noreply.github.com>
2025-02-07 21:04:26 +0500

    responsiveness milestone achieved V2

--- SocialSight/client/src/pages/Auth/Signin.jsx
+++ SocialSight/client/src/pages/Auth/Signin.jsx
@@ -42,7 +42,10 @@ function Signin() {
          <div className="flex flex-col justify-center items-center p-6 bg-white w-full md:w-1/2 h-full">
             <h2 className="text-3xl font-bold text-purple-700">Sign in</h2>
             <p className="text-gray-500 mt-1">Welcome to SocialSight</p>
-            <form className="w-full max-w-sm mt-6 space-y-4" onSubmit={handleSubmit}>
+            <form
+               className="w-full max-w-xs md:max-w-sm lg:max-w-md mt-4 md:mt-6 space-y-3 md:space-y-4 flex flex-col items-center"
+               onSubmit={handleSubmit}
+            >
                <input
                   type="email"
                   placeholder="Email Address"
@@ -59,7 +62,7 @@ function Signin() {
                />
                <button
                   type="submit"
-                  className="w-full bg-purple-600 hover:bg-purple-700 text-white font-bold py-3 rounded-lg"
+                  className="w-1/2 bg-purple-600 cursor-pointer hover:bg-purple-700 text-white font-bold py-3 rounded-full"
                >
                   SIGN IN
                </button>
@@ -67,7 +70,7 @@ function Signin() {
             <p className="mt-4 text-gray-600 ">
                New to SocialSight? &nbsp;
                <span
-                  className="text-purple-600 cursor-pointer hover:underline w-1/2 rounded-full py-2 text-center" 
+                  className="text-purple-600 cursor-pointer hover:underline w-1/2 rounded-full py-2 text-center"
                   onClick={() => navigate('/signup')}
                >
                   Sign up
